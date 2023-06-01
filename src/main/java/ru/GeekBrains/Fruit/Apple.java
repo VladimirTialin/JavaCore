c137db9398258e71889651f88ef1aaf101559d02
Vladimir <wel.tall@yandex.ru>
2023-06-01 16:44:21 +0300

    Задание выполнено

---
+++ src/main/java/ru/GeekBrains/Fruit/Apple.java
@@ -0,0 +1,10 @@
+package ru.GeekBrains.Fruit;
+
+    public class Apple extends Fruit {
+
+        @Override
+        public double weight() {
+            return 0.2;
+        }
+    }
+
---
+++ src/main/java/ru/GeekBrains/Fruit/Orange.java
@@ -0,0 +1,8 @@
+package ru.GeekBrains.Fruit;
+
+public class Orange extends Fruit {
+    @Override
+    public double weight() {
+        return 0.4;
+    }
+}
